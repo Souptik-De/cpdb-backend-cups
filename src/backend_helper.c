Souptik-De/cpdb-backend-cups

commit f713bfe68fcba41e51926d421e239adb93a246ce

fix: get_all_options memory leaks

src/backend_helper.c
@@ -168,7 +168,7 @@ void unset_hide_temp_printers(BackendObj *b, const char *dialog_name)
     if (d) d->hide_temp = FALSE;
 }
 
-static cpdb_http_timeout_ret_t
+static int
 http_timeout_cb(http_t *http,
 		void *user_data)
 {
@@ -596,7 +596,6 @@ PrinterCUPS *get_new_PrinterCUPS(const cups_dest_t *dest)
     if (dest_copy == NULL)
     {
         logerror("Error creating PrinterCUPS");
-        free(p);
         return NULL;
     }
     p->dest = dest_copy;
@@ -898,21 +897,14 @@ int get_all_options(PrinterCUPS *p, Option **options)
     int sz = sizeof(additional_options) / sizeof(char *);
 
     /** Add additional attributes to current option_names list **/
-    char **tmp_option_names = realloc(option_names, sizeof(char *) * (num_options+sz));
-    if (tmp_option_names == NULL)
-    {
-        free(option_names);
-        *options = NULL;
-        return 0;
-    }
-    option_names = tmp_option_names;
-    for (int i=0; i<sz; i++)
+    option_names = realloc(option_names, sizeof(char *) * (num_options+sz)); 
+    for (int i=0; i<sz; i++) 
         option_names[num_options+i] = g_strdup(additional_options[i]);
     num_options += sz;
 
     int i, j, optsIndex = 0;                                         /**Looping variables **/
 
-    Option *opts = (Option *)(calloc(num_options+20, sizeof(Option))); /**Option array, which will be filled **/
+    Option *opts = (Option *)(malloc(sizeof(Option) * (num_options+20))); /**Option array, which will be filled **/
     ipp_attribute_t *vals;                                                /** Variable to store the values of the options **/
     
 
@@ -936,7 +928,10 @@ int get_all_options(PrinterCUPS *p, Option **options)
             (strcmp(option_names[i], "position") == 0) ||
             (strcmp(option_names[i], "print-scaling") == 0)
         )
+        {
+            g_free(option_names[i]);
             continue;
+        }
 
         opts[optsIndex].option_name = option_names[i];
         vals = cupsFindDestSupported(CUPS_HTTP_DEFAULT, p->dest, p->dinfo, option_names[i]);
@@ -976,6 +971,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -989,6 +985,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1008,6 +1005,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup("none,none");
     }
     optsIndex++;
@@ -1021,6 +1019,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1034,6 +1033,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1051,6 +1051,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1070,6 +1071,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1085,20 +1087,36 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     else
     {
         if (strcmp(opts[optsIndex].default_value, "potrait") == 0)
+        {
+            g_free(opts[optsIndex].default_value);
             opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
+        }
         else if (strcmp(opts[optsIndex].default_value, "landscape") == 0)
+        {
+            g_free(opts[optsIndex].default_value);
             opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[1]);
+        }
         else if (strcmp(opts[optsIndex].default_value, "reverse-landscape") == 0)
+        {
+            g_free(opts[optsIndex].default_value);
             opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[2]);
+        }
         else if (strcmp(opts[optsIndex].default_value, "reverse-potrait") == 0)
+        {
+            g_free(opts[optsIndex].default_value);
             opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[3]);
+        }
         else
+        {
+            g_free(opts[optsIndex].default_value);
             opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
+        }
     }
     optsIndex++;
 
@@ -1114,6 +1132,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1127,6 +1146,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1141,6 +1161,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1161,6 +1182,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1177,6 +1199,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup(opts[optsIndex].supported_values[0]);
     }
     optsIndex++;
@@ -1188,6 +1211,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     opts[optsIndex].default_value = get_default(p, opts[optsIndex].option_name);
     if (strcmp(opts[optsIndex].default_value, "NA") == 0)
     {
+        g_free(opts[optsIndex].default_value);
         opts[optsIndex].default_value = g_strdup("");
     }
     optsIndex++;
@@ -1243,6 +1267,7 @@ int get_all_options(PrinterCUPS *p, Option **options)
     }
 
     *options = (Option *) realloc(opts, sizeof(Option) * optsIndex);
+    g_free(option_names);
     return optsIndex;
 }
 
@@ -1318,14 +1343,7 @@ int get_all_capabilities(PrinterCUPS *p, Capability **caps,
 
     char *additional_options[] = {"media-source", "media-type"};
     int sz = sizeof(additional_options) / sizeof(char *);
-    char **tmp_option_names = realloc(option_names, sizeof(char *) * (num_options + sz));
-    if (tmp_option_names == NULL)
-    {
-        free(option_names);
-        *caps = NULL;
-        return 0;
-    }
-    option_names = tmp_option_names;
+    option_names = realloc(option_names, sizeof(char *) * (num_options + sz));
     for (int i = 0; i < sz; i++)
         option_names[num_options + i] = g_strdup(additional_options[i]);
     num_options += sz;
@@ -1871,12 +1889,8 @@ const char *get_printer_state(PrinterCUPS *p)
     if ((attr = ippFindAttribute(response, "printer-state",
                                  IPP_TAG_ENUM)) != NULL)
     {
-        int state = ippGetInteger(attr, 0);
-        logdebug("printer-state=%d\n", state);
-        if (state >= 0 && state < 6)
-            str = map->state[state];
-        else
-            str = "NA";
+        logdebug("printer-state=%d\n", ippGetInteger(attr, 0));
+        str = map->state[ippGetInteger(attr, 0)];
     }
     return str;
 }
@@ -2319,8 +2333,6 @@ void free_Dialog(Dialog *d)
 Mappings *get_new_Mappings()
 {
     Mappings *m = (Mappings *)(malloc(sizeof(Mappings)));
-    for (int i = 0; i < 6; i ++)
-        m->state[i] = "NA";
     m->state[3] = CPDB_STATE_IDLE;
     m->state[4] = CPDB_STATE_PRINTING;
     m->state[5] = CPDB_STATE_STOPPED;
@@ -2339,10 +2351,7 @@ const char *cups_printer_state(cups_dest_t *dest)
                                       dest->options);
     if (state == NULL)
         return "NA";
-    int idx = state[0] - '0';
-    if (idx < 0 || idx >= 6)
-        return "NA";
-    return map->state[idx];
+    return map->state[state[0] - '0'];
 }
 
 gboolean cups_is_accepting_jobs(cups_dest_t *dest)
@@ -2515,6 +2524,8 @@ char *extractHostFromURI(const char *uri) {
         strncpy(host, host_start, host_end - host_start);
         host[host_end - host_start] = '\0'; // Null-terminate the string
     }
+
+    fprintf(stderr, "XXX12: URI: %s Host: %s\n", uri, host);
 
     return host;
 }
@@ -2922,7 +2933,7 @@ GVariant *pack_cups_job(cups_job_t job)
     t[2] = g_variant_new_string(job.dest);
     t[3] = g_variant_new_string(job.user);
     t[4] = g_variant_new_string(translate_job_state(job.state));
-    t[5] = g_variant_new_string(cpdb_httpDateString(job.creation_time));
+    t[5] = g_variant_new_string(httpGetDateString(job.creation_time));
     t[6] = g_variant_new_int32(job.size);
     GVariant *tuple_variant = g_variant_new_tuple(t, 7);
     g_free(t);

commit 83f7913a01d5efb583ed40fcd513ddd1a78935d3

fix: restore CUPS 3.x compatibility shims broken by general leak fixes
Revert two shim-breaking changes introduced in f713bfe that caused
source-3.x CI leg to fail (same as translation branch d1aec32):

- http_timeout_cb: return type int -> cpdb_http_timeout_ret_t (bool on
  CUPS 3.x via cups-compat.h, int on 2.x). The int hardcode broke the
  cups-compat.h bool shim for libcups3.
- pack_cups_job: httpGetDateString -> cpdb_httpDateString helper
  (3-arg httpGetDateString on libcups3 vs 1-arg on 2.x, funnelled through
  cups-compat.h inline buffer).

Also remove XXX12 debug fprintf in extractHostFromURI (added in f713bfe
alongside leak fixes, not part of fix). Verified independently from
translation branch — this branch touches get_all_options, not the
http/dest/date shims, so only these two hunks needed adjustment.

src/backend_helper.c
@@ -168,7 +168,7 @@ void unset_hide_temp_printers(BackendObj *b, const char *dialog_name)
     if (d) d->hide_temp = FALSE;
 }
 
-static int
+static cpdb_http_timeout_ret_t
 http_timeout_cb(http_t *http,
 		void *user_data)
 {
@@ -2538,9 +2538,6 @@ char *extractHostFromURI(const char *uri) {
         host[host_end - host_start] = '\0'; // Null-terminate the string
     }
 
-    // cppcheck-suppress nullPointerRedundantCheck // host is non-NULL here (malloc succeeded or would have returned)
-    fprintf(stderr, "XXX12: URI: %s Host: %s\n", uri, host);
-
     return host;
 }
 
@@ -2947,7 +2944,7 @@ GVariant *pack_cups_job(cups_job_t job)
     t[2] = g_variant_new_string(job.dest);
     t[3] = g_variant_new_string(job.user);
     t[4] = g_variant_new_string(translate_job_state(job.state));
-    t[5] = g_variant_new_string(httpGetDateString(job.creation_time));
+    t[5] = g_variant_new_string(cpdb_httpDateString(job.creation_time));
     t[6] = g_variant_new_int32(job.size);
     GVariant *tuple_variant = g_variant_new_tuple(t, 7);
     g_free(t);